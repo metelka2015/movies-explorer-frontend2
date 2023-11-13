94a79724b2677a1f0ed159a2db54654d2a2df06d
Elena <metelka2015@gmail.com>
2023-11-13 15:54:41 +0500

    feat: добавить элемент promo

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,17 +1,19 @@
 import React from "react";
 import './Promo.css';
+import promo from "../../images/promo-image.svg";
 
 export function Promo () {
     return (
         <section className="promo">
             <div className="promo__container">
                 <div className="promo__titles">
-                    <h1 className="promo_title">Учебный проект студента факультета Веб-разработки.</h1>
+                    <h1 className="promo__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
                 <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
                 </div>
-                <img className="promo__picture" alt="рисунок планеты" src="" />
-            </div>
-            
+                <div className="promo__image-container">
+                    <img className="promo__image" alt="рисунок планеты" src={promo} />
+                </div>                
+            </div>          
             
             <a href="#know" className="promo__link">Узнать больше</a>
         </section>
